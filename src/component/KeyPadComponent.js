5dd5ab953f32542e42d8fe3c6e2c4c4148fcd5a0
nataliejuner <nataliereinicke@gmail.com>
2019-05-01 16:33:01 -0700

    css styling

--- src/component/KeyPadComponent.js
+++ src/component/KeyPadComponent.js
@@ -1,40 +1,40 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 class KeyPadComponent extends React.Component{
 	render(){
 		return(
 			// CE is to backspace by one
 		<div className="button">
-			<button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-			<button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-			<button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
-			<button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br />
+			<Button  name="(" onClick={e => this.props.onClick(e.target.name)}>(</Button>
+			<Button name=")" onClick={e => this.props.onClick(e.target.name)}>)</Button>
+			<Button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</Button>
+			<Button name="C" onClick={e => this.props.onClick(e.target.name)}>C</Button><br />
 
 
-			<button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-			<button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
-			<button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
-			<button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br />
+			<Button name="1" onClick={e => this.props.onClick(e.target.name)}>1</Button>
+			<Button name="2" onClick={e => this.props.onClick(e.target.name)}>2</Button>
+			<Button name="3" onClick={e => this.props.onClick(e.target.name)}>3</Button>
+			<Button name="+" onClick={e => this.props.onClick(e.target.name)}>+</Button><br />
 
 
-			<button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
-			<button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
-			<button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
-			<button name="-" onClick={e => this.props.onClick(e.target.name)}>-</button><br />
+			<Button name="4" onClick={e => this.props.onClick(e.target.name)}>4</Button>
+			<Button name="5" onClick={e => this.props.onClick(e.target.name)}>5</Button>
+			<Button name="6" onClick={e => this.props.onClick(e.target.name)}>6</Button>
+			<Button name="-" onClick={e => this.props.onClick(e.target.name)}>-</Button><br />
 
-			<button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
-			<button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
-			<button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
-			<button name="*" onClick={e => this.props.onClick(e.target.name)}>x</button><br />
+			<Button name="7" onClick={e => this.props.onClick(e.target.name)}>7</Button>
+			<Button name="8" onClick={e => this.props.onClick(e.target.name)}>8</Button>
+			<Button name="9" onClick={e => this.props.onClick(e.target.name)}>9</Button>
+			<Button name="*" onClick={e => this.props.onClick(e.target.name)}>x</Button><br />
 
-
-			<button name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
-			<button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
-			<button name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
-			<button name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br />
+			<Button name="." onClick={e => this.props.onClick(e.target.name)}>.</Button>
+			<Button name="0" onClick={e => this.props.onClick(e.target.name)}>0</Button>
+			<Button name="=" onClick={e => this.props.onClick(e.target.name)}>=</Button>
+			<Button name="/" onClick={e => this.props.onClick(e.target.name)}>÷</Button><br />
 		</div>
 		);
 	}
 }
 
-export default KeyPadComponent;
+export default KeyPadComponent
